504a4eb0a3d8a67af372fc94dfdd2cb134c74fa8
root <nicholasg101@gmail.com>
2023-08-25 22:11:22 +0000

    batching works

--- model_server.py
+++ model_server.py
@@ -7,12 +7,14 @@ prompts = [
     "The capital of France is",
     "The future of AI is",
 ]
+many_prompts = prompts * 2
+
 
 model_dir = "/src/models/TheBloke_Llama-2-13B-chat-GPTQ_gptq-4bit-32g-actorder_True"
-model = ExllamaModel(model_dir, batch_size=4)
+model = ExllamaModel(model_dir, batch_size=8)
 
-model.generate(prompts)
-
+out = model.generate(many_prompts)
+print(out)
 
 
 
