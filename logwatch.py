29688a81918216ca5b85b742347cea1fe0541e6b
root <nicholasg101@gmail.com>
2023-09-08 22:08:34 +0000

    got regexs to identify num_running

--- logwatch.py
+++ logwatch.py
@@ -5,27 +5,26 @@ import argparse
 import json
 
 def extract_token_details(log_line):
-    tokens_per_second_pattern = re.compile(r'(\d+\.\d+) tokens/s')
+    tokens_per_second_pattern = re.compile(r'Avg generation throughput: (\d+\.\d+) tokens/s')
     tokens_per_second_match = tokens_per_second_pattern.search(log_line)
     tokens_per_second = float(tokens_per_second_match.group(1)) if tokens_per_second_match else None
     return tokens_per_second
 
 #Can add pending too
 def extract_running_details(log_line):
-    running_pattern = re.compile(r'Running: \d reqs')
+    running_pattern = re.compile(r'Running: (\d+) reqs')
     running_match = running_pattern.search(log_line)
     num_running = int(running_match.group(1)) if running_match else None
     return num_running
 
-def notify_control_server(line, message, control_server_url):
-    data = json.loads(message)
-    data["line"] = line
+def notify_control_server(line, data_dict, control_server_url):
+    data = json.loads(data_dict)
 
-    if ("tokens/s" in line):
+    if ("Avg generation throughput:" in line):
         tokens_per_second = extract_token_details(line)
         data["tokens/s"] = tokens_per_second
 
-    if "Running " in line:
+    if "Running:" in line:
         num_running = extract_running_details(line)
         data["num_running"] = num_running
 
@@ -49,7 +48,7 @@ def main():
         print(line)
         for pattern in args.patterns:
             if re.search(pattern, line):
-                notify_control_server(line.strip(), args.message, args.control_server_url)
+                notify_control_server(line.strip(), args.data_dict, args.control_server_url)
                 break
     print("done iterating through stdin")
 
