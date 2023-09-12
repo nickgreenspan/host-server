4767421d4c67bf0eea9ad7dcf0d1e872d416f47b
root <nicholasg101@gmail.com>
2023-09-12 22:57:52 +0000

    compatability with huggingface TGI mostly complete

---
+++ auth_server_hf_tgi.py
@@ -0,0 +1,54 @@
+from flask import Flask, Response, request, abort
+import os
+import secrets
+import requests
+import logging
+import time
+
+app = Flask(__name__)
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+mtoken = os.environ['MASTER_TOKEN']
+HF_SERVER = '127.0.0.1:5001'
+BATCH_SIZE = 1000
+curr_tokens = set()
+
+@app.route('/tokens', methods=['GET'])
+def get_tokens():
+    global mtoken, curr_tokens
+    if request.json['mtoken'] != mtoken:
+        abort(401)
+    new_tokens = gen_tokens()
+    curr_tokens |= set(new_tokens)
+    return {"tokens" : new_tokens}
+
+def gen_tokens():
+    token_batch = []
+    for _ in range(BATCH_SIZE):
+        token = secrets.token_hex(32)
+        token_batch.append(token)
+    return token_batch
+
+def hf_tgi_wrapper(text_prompt):
+    hf_prompt = {"inputs" : text_prompt, "parameters" : {"max_new_tokens" : 200}}
+    response = requests.post(f"http://{HF_SERVER}/generate_stream", json=hf_prompt, stream=True)
+    if response.status_code == 200:
+        for byte_payload in response.iter_lines():
+            yield byte_payload
+            yield "\n"
+
+@app.route('/connect', methods=['POST'])
+def auth():
+    global mtoken, curr_tokens, tgi_client
+    token = request.json['token']
+    if token in curr_tokens:
+        curr_tokens.remove(token)
+    elif token != mtoken:
+        abort(401)
+
+    return Response(hf_tgi_wrapper(request.json['prompt']))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, threaded=True)
--- logwatch_json.py
+++ logwatch_json.py
@@ -4,26 +4,34 @@ import requests
 import argparse
 import json
 
-def extract(line_json, pattern_str, pattern_expression, data):
-    match = pattern_expression.search(log_line)
-    if match:
-        value = float(match.group(1))
-        data[pattern_str] = value
-        return True
-    
-    return False
 
-def forward_server_data(line_json, extract_pattern_dict, data_dict, control_server_url):
+
+def format_metric_value(metric_str):
+    if metric_str[-2:] == "ms":
+        return (float(metric_str[:-2]) / 10.0e3)
+
+    elif ord(metric_str[-2]) == 181: #mu
+        return (float(metric_str[:-2]) / 10.0e6)
+
+    elif metric_str[-1:] == "s":
+        return (float(metric_str[:-1]))
+
+    else:
+        return metric_str
+
+def forward_server_data(line_metrics, metric_names, data_dict, control_server_url):
     data = json.loads(data_dict)
 
     found = False
-    for pattern_str, pattern_expression in extract_pattern_dict.items():
-        found |= extract(line, pattern_str, pattern_expression, data)
+   
+    for metric_name in metric_names:
+        if metric_name in line_metrics.keys():
+            data[metric_name] = format_metric_value(line_metrics[metric_name])
+            found = True
 
     if found:
         print(f'sending data to url: {control_server_url}, data: {data}')
-        response = requests.post(control_server_url, json = data)
-        # print(f"Notification sent. Response: {response.status_code}")
+        # response = requests.post(control_server_url, json = data)
         print(f"Notification sent.") #response.status_code
 
 def notify_server_ready(data_dict, control_server_url):
@@ -44,19 +52,16 @@ def main():
 
     args = parser.parse_args()
 
-    print("[logwatch] starting compiling")
- 
-    # if args.backend == "hf_tgi":
-    ready_pattern = re.compile(r"Connected") # INFO text_generation_router: router/src/main\.rs:247: 
-    extract_pattern_dict = {"inference_time": re.compile(r'inference_time="(\d+\.\d+)s"'), "queue_time" : re.compile(r'queue_time="(\d+\.\d+)ms"'), "time_per_token" : re.compile(r'time_per_token="(\d+\.\d+)ms"')}
+    metric_names = ["time_per_token", "inference_time", "queue_time", "validation_time"]
 
     print("[logwatch] ready and waiting for input")
     for line in sys.stdin:
         line_json = json.loads(line)
-        if line_json["message"] == "Connected" and line_json["target"] == "text_generation_router":
-            notify_server_ready(args.data_dict, args.control_server_url)
-        elif line_json["message"] == "Success" and line_json["target"] == "text_generation_router::server":
-            forward_server_data(line_json, extract_pattern_dict, args.data_dict, args.control_server_url)
+        if "message" in line_json.keys():
+            if line_json["message"] == "Connected" and line_json["target"] == "text_generation_router":
+                notify_server_ready(args.data_dict, args.control_server_url)
+            elif line_json["message"] == "Success" and line_json["target"] == "text_generation_router::server":
+                forward_server_data(line_json["span"], metric_names, args.data_dict, args.control_server_url)
 
 
 if __name__ == "__main__":
