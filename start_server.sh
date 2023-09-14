d164eb67bbdc40e06c13566a68c18be0db67342b
root <nicholasg101@gmail.com>
2023-09-14 20:01:15 +0000

    added print statements

--- start_server.sh
+++ start_server.sh
@@ -5,6 +5,7 @@ export SERVER_DIR="/usr/src/host-server" #want a better system, but necessary fo
 export PATH="/opt/conda/bin:$PATH"
 export MASTER_TOKEN="mtoken" #hack for this instance
 export REPORT_ADDR=$1
+echo $REPORT_ADDR
 
 source "$SERVER_DIR/start_watch.sh"
 source "$SERVER_DIR/launch_model.sh"
